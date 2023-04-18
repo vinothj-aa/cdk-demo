fb01734cc53fdb3d0d3661a74946a507865fb7a9
Vinoth J <vinothj_aa@yahoo.co.in>
2023-04-19 01:38:46 +0530

    Updated references for cdk due to an error

--- bin/cdk-demo.ts
+++ bin/cdk-demo.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { CdkDemoStack } from '../lib/cdk-demo-stack';
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+import { CdkDemoStack } from "../lib/cdk-demo-stack";
 
 const app = new cdk.App();
-new CdkDemoStack(app, 'CdkDemoStack', {
+new CdkDemoStack(app, "CdkDemoStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -15,7 +15,7 @@ new CdkDemoStack(app, 'CdkDemoStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: "688098623206", region: "ap-south-1" },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- lib/cdk-demo-stack.ts
+++ lib/cdk-demo-stack.ts
@@ -1,16 +1,21 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "@aws-cdk/core";
+import * as ec2 from "@aws-cdk/aws-ec2";
 
 export class CdkDemoStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new ec2.Vpc(this, "mainVPC", {
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: "public-subnet",
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+      ],
+    });
   }
 }
